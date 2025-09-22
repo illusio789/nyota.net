18ddf47849e2b9e18b2859dbd8dbcc879646ce31
Ian Hill <ian.hill1@gmail.com>
2025-09-22 18:52:53 +0100

    Refactor FileRunConfigRepository to use IFileSystem for file operations

--- src/Nyota.Storage.File/FileRunConfigRepository.cs
+++ src/Nyota.Storage.File/FileRunConfigRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.IO.Abstractions;
+using System.Text.Json;
 
 using Nyota.Abstractions;
 using Nyota.Domain;
@@ -7,9 +8,17 @@ namespace Nyota.Storage.File;
 
 public sealed class FileRunConfigRepository : IRunConfigRepository
 {
+    private readonly IFileSystem _fileSystem;
+
+    public FileRunConfigRepository(IFileSystem fileSystem)
+    {
+        _fileSystem = fileSystem;
+    }
+
+
     public async Task<BacktestConfig> LoadAsync(string pathOrId, CancellationToken ct)
     {
-        var json = await System.IO.File.ReadAllTextAsync(pathOrId, ct);
+        var json = await _fileSystem.File.ReadAllTextAsync(pathOrId, ct);
         return JsonSerializer.Deserialize<BacktestConfig>(json, new JsonSerializerOptions{})!;
     }
 }
